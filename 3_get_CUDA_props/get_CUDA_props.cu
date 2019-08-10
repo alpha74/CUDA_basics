116ff00e00e363a1e2a5b03ece58868284772388
Aman Kumar <amanalphakumar@gmail.com>
2019-08-10 18:33:42 +0530

    Update get_CUDA_props.cu

--- 3_get_CUDA_props/get_CUDA_props.cu
+++ 3_get_CUDA_props/get_CUDA_props.cu
@@ -1,4 +1,5 @@
-// Getting information about CUDA
+// Getting information about CUDA Device
+// Author: alpha74
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
@@ -10,9 +11,11 @@ int main()
 	cudaDeviceProp prop;
 
 	int devcount;
-
+	
+	// Returns the number of CUDA devices attached to system
 	cudaGetDeviceCount(&devcount);
 
+	// Iterate and fetch the details of each deviceID
 	for (int i = 0; i < devcount; i++)
 	{
 		cudaGetDeviceProperties(&prop, i);
